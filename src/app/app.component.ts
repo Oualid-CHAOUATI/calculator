baedf762039ff82b2d44c860f7dbc788d7295a7c
waleed <oualidchaouati3@gmail.com>
2023-03-11 13:34:20 +0100

    first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class AppComponent {
-  title = 'calculator';
+export class AppComponent implements OnInit {
+  result: number = 0;
+  screen: string = '';
+  modeNumber = 0;
+
+  mode: string = '+';
+  ngOnInit(): void {
+    this.init();
+  }
+
+  init() {
+    this.screen = '';
+  }
+
+  attachToScreen(s: string | number) {
+    this.screen += `${s}`;
+  }
+
+  setMode(mode: string) {
+    const oldValue = this.result;
+    const newValue = parseFloat(this.screen);
+    if (mode == 'c') {
+      this.screen = '';
+      this.result = 0;
+    } else if (mode == '=') {
+      this.screen = '';
+      this.calc(oldValue, newValue);
+      this.attachToScreen(this.result);
+    } else {
+      this.mode = mode;
+      this.result = parseFloat(this.screen);
+      this.screen = '';
+    }
+  }
+
+  calc(a: number, b: number) {
+    switch (this.mode) {
+      case '=':
+        this.result = this.result;
+        break;
+
+      case '+':
+        this.result = a + b;
+        break;
+      case '-':
+        this.result = a - b;
+        break;
+
+      case '*':
+        this.result = a * b;
+        break;
+      case '/':
+        this.result = a / b;
+        break;
+    }
+  }
 }
